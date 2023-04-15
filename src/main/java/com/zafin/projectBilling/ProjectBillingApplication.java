8e10cf946fe20d17d622893418c123db1ebfb01b
SnehaRoy <snehathankomroy@gmail.com>
2023-04-15 07:44:44 +0530

    created a spring project

---
+++ src/main/java/com/zafin/projectBilling/ProjectBillingApplication.java
@@ -0,0 +1,13 @@
+package com.zafin.projectBilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zafin/projectBilling/ProjectBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zafin.projectBilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
